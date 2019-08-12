a7429dbfdc8ea89ed8145317f2d655e4a49778ea
Maiya Tracy <maiya.tracy@gmail.com>
2019-08-12 15:40:56 -0700

    ninja gold

--- ninjagold/src/main/java/com/maiya/ninjagold/control/Control.java
+++ ninjagold/src/main/java/com/maiya/ninjagold/control/Control.java
@@ -1,7 +1,7 @@
 package com.maiya.ninjagold.control;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.Random;
 
@@ -9,38 +9,42 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class Control {
 	@RequestMapping("/")
-	public String index(HttpSession session) {
+	public String index(HttpSession session, Model model) {
+		DecimalFormat df2 = new DecimalFormat("#.##");
 		if (session.getAttribute("gold") == null) {
 			session.setAttribute("gold", 0);
 		}
+		model.addAttribute("gold", df2.format(session.getAttribute("gold")));
+		ArrayList<String[]> activitiesarr = (ArrayList<String[]>) session.getAttribute("activities");
+		model.addAttribute("activities", activitiesarr);
 		return "index.jsp";
 	}
 
-	@RequestMapping(value = "/process_money", method = RequestMethod.POST)
-	public String login(HttpSession session, @RequestParam(value = "location") String location) {
-		double earnings = 0;
+	@RequestMapping(value = "/process_money/{location}", method = RequestMethod.POST)
+	public String login(HttpSession session, @PathVariable("location") String location) {
+		Double earnings = 0.0;
 		Date today = new java.util.Date();
-		if (location == "farm") {
+		if (location.equals("farm")) {
 			earnings = returnRandomNumber(10, 20);
+			System.out.println(earnings);
 			addToSessionActivities(session, "Earned " + String.valueOf(earnings) + " golds from the farm! " + today,
 					"good");
-		} else if (location == "cave") {
+		} else if (location.equals("cave")) {
 			earnings = returnRandomNumber(5, 10);
 			addToSessionActivities(session, "Earned " + String.valueOf(earnings) + " golds from the cave!" + today,
 					"good");
-		} else if (location == "house") {
+		} else if (location.equals("house")) {
 			earnings = returnRandomNumber(2, 5);
 			addToSessionActivities(session, "Earned " + String.valueOf(earnings) + " golds from the house!" + today,
 					"good");
-		} else if (location == "casino") {
+		} else if (location.equals("casino")) {
 			earnings = returnRandomNumber(-50, 50);
 			if (earnings >= 0) {
 				addToSessionActivities(session, "Won " + String.valueOf(earnings) + " golds from the casino!" + today,
@@ -49,10 +53,11 @@ public class Control {
 				addToSessionActivities(session,
 						"Entered a casino and lost " + String.valueOf(earnings * -1) + " golds... Ouch!" + today,
 						"bad");
-				addToTotalGold(session, earnings);
 			}
 		}
-		return "redirect:/success";
+		addToTotalGold(session, earnings);
+
+		return "redirect:/";
 
 	}
 
@@ -63,28 +68,42 @@ public class Control {
 		return "redirect:/";
 	}
 
-	public double addToTotalGold(HttpSession session, double amount) {
+	public Double addToTotalGold(HttpSession session, Double amount) {
 		if (session.getAttribute("gold") == null) {
 			session.setAttribute("gold", amount);
+			System.out.println("adding to gold - not in session");
+			System.out.println(amount);
+			System.out.println(session.getAttribute("gold"));
 		} else {
-			double new_amount = (double) session.getAttribute("gold") + amount;
+			Double new_amount;
+			if (session.getAttribute("gold").equals(0)) {
+				new_amount = ((Integer) session.getAttribute("gold")).doubleValue() + amount;
+			} else {
+				new_amount = (Double) session.getAttribute("gold") + amount;
+			}			
 			session.setAttribute("gold", new_amount);
+			System.out.println("adding to gold - in session");
+			System.out.println(amount);
+			System.out.println(session.getAttribute("gold"));
 		}
 		return amount;
 	}
 
-	public double returnRandomNumber(int low, int high) {
+	public Double returnRandomNumber(int low, int high) {
 		Random r = new Random();
-		double new_num = low + (double) r.nextInt(high - low);
+		Double new_num = low + (high - low) * r.nextDouble();
 		return new_num;
 	}
 
 	public void addToSessionActivities(HttpSession session, String message, String goodorbad) {
 		if (session.getAttribute("activities") == null) {
-			session.setAttribute("activities", new ArrayList<String[]>);
+			session.setAttribute("activities", new ArrayList<String[]>());
 		}
-		String[] message_arr = {goodorbad, message};
-	    session.setAttribute("activities", session.getAttribute("activities").add(message_arr));
+		String[] message_arr = { goodorbad, message };
+		ArrayList<String[]> activities_arr = (ArrayList<String[]>) session.getAttribute("activities");
+		System.out.println(activities_arr);
+		activities_arr.add(message_arr);
+		session.setAttribute("activities", activities_arr);
 
 	}
 }
--- ninjagold/src/main/webapp/WEB-INF/index.jsp
+++ ninjagold/src/main/webapp/WEB-INF/index.jsp
@@ -14,47 +14,60 @@
 <title>Insert title here</title>
 </head>
 <body>
-<c:out value="${error}" />
+	<c:out value="${error}" />
 	<div class="container">
-    <div class="header row">
-      <label for="your_gold">Your Gold:</label>
-      <input id='your_gold' disabled type="text" value=<c:out value="${request.session.total_gold}"/>>
-    </div>
-    <div class="boxes row">
-      <div class="farm border col-3">
-        <form action="/ninja_gold/process_money/farm" method="post">
-          <h3>Farm</h3>
-          <h6>(earns 10-20 golds)</h6>
-          <input style="btn btn-outline-primary" type="submit" value="Find Gold!">
-        </form>
-      </div>
-      <div class="cave border col-3">
-        <form action="/ninja_gold/process_money/cave" method="post">
-          <h3>Cave</h3>
-          <h6>(earns 5-10 golds)</h6>
-          <input style="btn btn-outline-primary" type="submit" value="Find Gold!">
-        </form>
-      </div>
-      <div class="house border col-3">
-        <form action="/ninja_gold/process_money/house" method="post">
-          <h3>House</h3>
-          <h6>(earns 2-5 golds)</h6>
-          <input style="btn btn-outline-primary" type="submit" value="Find Gold!">
-        </form>
-      </div>
-      <div class="casino border col-3">
-        <form action="/ninja_gold/process_money/casino" method="post">
-          <h3>Casino</h3>
-          <h6>(earns/takes 0-50 golds)</h6>
-          <input style="btn btn-outline-primary" type="submit" value="Find Gold!">
-        </form>
-      </div>
-    </div>
-    <div class="footer row">
-      <label for="activities_box">Activities: </label>
-      
-    </div>
-    <form action="/ninja_gold/clear" method="POST"><input type="submit" value="Clear game" class="btn btn-outline-danger"></form>
-  </div>
+		<div class="header row">
+			<label for="your_gold">Your Gold:</label> <input id='your_gold'
+				disabled type="text" value=<c:out value="${gold}"/>>
+		</div>
+		<div class="boxes row">
+			<div class="farm border col-3">
+				<form action="/process_money/farm" method="post">
+					<h3>Farm</h3>
+					<h6>(earns 10-20 golds)</h6>
+					<input style="" type="submit" value="Find Gold!">
+				</form>
+			</div>
+			<div class="cave border col-3">
+				<form action="/process_money/cave" method="post">
+					<h3>Cave</h3>
+					<h6>(earns 5-10 golds)</h6>
+					<input style="" type="submit" value="Find Gold!">
+				</form>
+			</div>
+			<div class="house border col-3">
+				<form action="/process_money/house" method="post">
+					<h3>House</h3>
+					<h6>(earns 2-5 golds)</h6>
+					<input style="" type="submit" value="Find Gold!">
+				</form>
+			</div>
+			<div class="casino border col-3">
+				<form action="/process_money/casino" method="post">
+					<h3>Casino</h3>
+					<h6>(earns/takes 0-50 golds)</h6>
+					<input style="" type="submit" value="Find Gold!">
+				</form>
+			</div>
+		</div>
+		<div class="footer row">
+			<label for="activities_box">Activities: </label>
+			<textarea name="activities_box" id="activities_box" rows="4" cols="500">
+				<c:forEach items="${activities}" var="activity">
+				<c:out value="${activity}" />
+					<c:if test="${activity[0] = good}">
+						<p class="text-danger"><c:out value="${activity[0]}" /></p>
+					</c:if>
+					<c:if test="${activity[0] = bad}">
+						<p class="text-danger"><c:out value="${activity[1]}" /></p>
+					</c:if>
+				</c:forEach>
+			</textarea>
+		</div>
+		<form action="/clear" method="POST">
+			<input type="submit" value="Clear game"
+				class="btn btn-outline-danger">
+		</form>
+	</div>
 </body>
 </html>
